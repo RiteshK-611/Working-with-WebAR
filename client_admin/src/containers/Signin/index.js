f56511a0d4e0f3f76376b4d3885d3d603cf14ea5
unknown <bsmhatre888@gmail.com>
2021-05-10 11:47:41 +0530

    4th video completed

---
+++ client_admin/src/containers/Signin/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import Input from '../../components/UI/Input';
+/**
+ * @author
+ * @returns 
+ **/
+
+const Signin = (props) => {
+    return (
+        <Layout>
+            <Container>
+                <Row style= {{marginTop: '50px'}}>
+                    <Col md={{span: 6, offset: 3}}>
+                        <Form>
+                        <Input
+                                label="Email"
+                                placeholder="Email"
+                                value=""
+                                type="email"
+                                onChange={() => { }}
+                            />
+
+                        <Input
+                                label="Password"
+                                placeholder="Password"
+                                value=""
+                                type="password"
+                                onChange={() => { }}
+                            />
+                            
+                            <Button variant="primary" type="submit">
+                                Submit
+                </Button>
+                        </Form>
+                    </Col>
+                </Row>
+
+
+            </Container>
+        </Layout>
+    )
+
+}
+
+export default Signin
